f2eb2a76662e4cdba282ce4fe1890806140dbae4
hayesejasmine <hayesejasmine@gmail.com>
2022-11-06 13:44:04 -0500

    HTML and CSS updated, created each page for the quiz, will update javascript next to make them funtionable

---
+++ Assets/script.js
@@ -0,0 +1,42 @@
+//Make quiz start when the start button is selected
+
+//Make header invisible and fist quiz question visible
+
+//Make timer run, ensuring that time decreses by 10 seconds if answered wrong
+
+//Make second quiz question visible
+
+
+
+
+
+
+//Commonly used data types DO Not Include:
+//strings
+//booleans
+//alerts
+//numbers
+
+//The condition in an if/else statement is inclosed with ____________.
+//quotes
+//curly brackets
+//parenthesis
+//square brackets
+
+//Arrays in JavaScript can be used to store ________________.Arrays
+//numbers and strings
+//other arrays
+//booleans
+//all of the above
+
+//String values must be enclosed within __________________ when being assigned to variables.
+//commas
+//curly brackets
+//quotes
+//parenthesis
+
+//A very useful tool used during development and debugging for printing content to the debugger is:
+// JavaScript
+//terminal/bash
+//for loops
+//console.log
